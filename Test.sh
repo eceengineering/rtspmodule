39bfd2d2fa9c0ae897bade65ec36338be4912d04
ECE ENGINEERING <ozgur.eralp@outlook.com>
2014-12-24 13:14:40 +0000

    Create Test.sh

---
+++ Test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -e
+echo "-------------------------------------------------------------------"
+echo " THIS IS A TEST APPLICATION NAMED BBWATCH - BABY WATCH " 
+echo " ASSUMPTIONS:"
+echo "	- Camera able to capture YUV (UYVY) format,"
+echo "	- GStreamer 0.10 installed."
+echo "-------------------------------------------------------------------"
+
+sudo ./bbwatch
